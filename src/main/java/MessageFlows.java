665f95722489a4911912796a324a2ba71e8065e9
Andreas Biberacher <andreas-biberacher@gmx.de>
2018-01-18 23:26:21 +0100

    Variablenübergabe mit Message und Anpassung der UserForms

--- src/main/java/MessageFlows.java
+++ src/main/java/MessageFlows.java
@@ -1,4 +1,5 @@
 
+import java.util.Map;
 import java.util.logging.Logger;
 
 import org.camunda.bpm.engine.RuntimeService;
@@ -6,45 +7,45 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 
-
-
 public class MessageFlows implements JavaDelegate{
 
 
 
-    private final Logger LOGGER = Logger.getLogger(MessageFlows.class.getName());
-
+    private final Logger LOGGER = Logger.getAnonymousLogger();
 
     public void execute (DelegateExecution execution) throws Exception {
 
         RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
-        runtimeService.startProcessInstanceByMessage("Message1");
+
+        String sendMessage="";
+
+        switch (execution.getCurrentActivityId()){
+            case "SendTask_0rshnh8":
+                sendMessage = "Message1";
+                break;
+            case "Task_12d1s5n":
+                sendMessage = "Message_Engine_BudgetPruefen";
+                break;
+        }
+        Map<String,Object> processVariables = execution.getVariables();
+
+        processVariables.forEach((s,o) -> LOGGER.info("String: " + s +"\tObject: " + o + " \n")
+        );
+
+
+        runtimeService.startProcessInstanceByMessage(sendMessage,processVariables);
 
 
         LOGGER.info("\n\n  ... LoggerDelegate invoked by "
                 + "processDefinitionId=" + execution.getProcessDefinitionId()
-                + ", activtyId=" + execution.getCurrentActivityId()
-                + ", activtyName='" + execution.getCurrentActivityName() + "'"
+                + ", activityId=" + execution.getCurrentActivityId()
+                + ", activityName='" + execution.getCurrentActivityName().replaceAll("\n", " ") + "'"
                 + ", processInstanceId=" + execution.getProcessInstanceId()
                 + ", businessKey=" + execution.getProcessBusinessKey()
                 + ", executionId=" + execution.getId()
+                + ", modelName=" + execution.getBpmnModelInstance().getModel().getModelName()
+                + ", elementId" + execution.getBpmnModelElementInstance().getId()
                 + " \n\n");
 
-
-
-//        runtimeService.startProcessInstanceByMessage("Message_Mitarbeiter_AntragAusfuellen");
     }
-
-
-/*
-    public void message_Mitarbeiter_AntragAusfuellen_execute(DelegateExecution execution) throws Exception {
-            RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
-            runtimeService.startProcessInstanceByMessage("Message_Mitarbeiter_AntragAusfuellen");
-    }
-*/
-
-
-
-
-
 }
