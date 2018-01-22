88678c22f343a609b04a81541bc1ada63b068cfd
Marcus Danzer <danzer.marcus@gmail.com>
2018-01-22 13:05:51 +0100

    Changed bpmn model

--- src/main/java/Mail.java
+++ src/main/java/Mail.java
@@ -11,7 +11,7 @@ public class Mail {
     private static final String SMTP_HOST_NAME = "smtp.gmail.com";
     private static final int SMTP_HOST_PORT = 465;
     private static final String SMTP_AUTH_USER = "bpm.mail.sender@gmail.com";
-    private static final String SMTP_AUTH_PWD  = "start12345";
+    private static final String SMTP_AUTH_PWD = "start12345";
 
     public void sendMailTo(String receiver, String customer, String start, String end, String employeeId) throws Exception {
         Properties props = new Properties();
@@ -28,19 +28,8 @@ public class Mail {
         MimeMessage message = new MimeMessage(mailSession);
 
         message.setSubject("Sie haben eine neue Reisekostenabrechnung für den Customer " + customer + " von " + employeeId);
-        message.setContent("test", "text/plain");
-
-        final Logger LOGGER = Logger.getAnonymousLogger();
-
-        LOGGER.info("\n\n  EMAIL LOGGING " +
-                        "\nresiver: " + receiver +
-                        "\ncustomer: " + customer +
-                        "\nemployeeId: " + employeeId  );
-
-
-/*        message.setSubject("Sie haben eine neue Reisekostenabrechnung für den Customer " + customer + " von " + employeeId);
         message.setContent("Sehr geehrter Herr Demo, \nHerr Demo hat eine neue Reisenkostenabrechnung abgegeben für den Zeitraum vom "
-                +start +" bis "+ end + "\n Diese E-Mail ist automatisch generiert.", "text/plain");*/
+                + start + " bis " + end + "\n\nDiese E-Mail ist automatisch generiert.", "text/plain");
 
         message.addRecipient(Message.RecipientType.TO,
                 new InternetAddress(receiver));
